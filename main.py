ba5c4a3435f89f6a46a708f5a109615cd341c8f0
Deciptikon <deciptikon@mail.ru>
2024-01-04 02:29:40 +0300

    Прохождение сигнала по Link с учетом торможения

--- main.py
+++ main.py
@@ -43,11 +43,29 @@ class Link:
         self.T_links: list[list[Link]] = []
     
     def step(self) -> None:
-        self.link = [False] + self.link[:-1]
-        
-    def step2(self) -> None:
         self.link.insert(0, False)
         self.link.pop()
+        
+        print(len(self.T_links))
+        '''Проверяем не пуст ли список тормозных аксонов'''
+        if len(self.T_links) > 0:
+            
+            '''Для каждого набора присоединённых к этой ячейке
+            тормозных аксонов'''
+            for i in range(0, len(self.link) - 1):
+                T_links = self.T_links[i]
+                '''Проверяем не пуст ли список прикрепеленных тормозных аксонов'''
+                if len(T_links) > 0:
+                    '''проходим в каждый аксон'''
+                    for T in T_links:
+                        '''И если спайк в нем достиг точки крепления'''
+                        if T.get_last_value():
+                            '''тормозим сигнал в нормальном аксоне
+                            в точке крепления'''
+                            self.link[i] = False
+                
+            
+        
     
     def print(self, prefix: str = ''):
         print(f'{prefix}{self.link}')
@@ -58,9 +76,20 @@ class Link:
     def set_length(self, length: int = 1) -> None:
         self.link = [False] * length
         
-    def set_T_link(self, link: 'Link' = None, position: int = 0):
+    def set_T_link(self, T_link: 'Link' = None, position: int = 0):
         if position < len(self.link):
-            pass
+            if len(self.T_links) == 0:
+                self.T_links = [[]]*len(self.link)
+            print(self.T_links)
+            if len(self.T_links[position]) == 0:
+                self.T_links[position] = [T_link]
+                print(self.T_links)
+            else:
+                self.T_links[position].append(T_link)
+    
+    def get_last_value(self) -> bool:
+        return self.link[len(self.link)-1]
+        
     
 class Neuron:
     def __init__(self) -> None:
@@ -81,6 +110,8 @@ T_link.link.insert(2, True)
 link.print(prefix='link   = ')
 T_link.print(prefix='T-link = ')
 
+link.set_T_link(T_link=T_link, position=2)
+link.set_T_link(T_link=T_link, position=5)
 
 link.step()
 T_link.step()
