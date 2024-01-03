ad647bdbd8e20d564ec756d349d3c76fdf4e0f88
Deciptikon <deciptikon@mail.ru>
2024-01-03 10:56:56 +0300

    Добавлены абстрактные классы

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print(f'Test 4 8 15 16 23 42')
+from abc import ABC, abstractmethod
+
+class AbstractLayer(ABC):
+    
+    @abstractmethod
+    def forward(self, input_data: list) -> list:
+        pass
+    
+    ''' Этот метод под вопросом '''
+    @abstractmethod
+    def backward(self, gradient):
+        pass
+    
+
+class AbstractSNN(ABC):
+    
+    @abstractmethod
+    def train(self, input_data: list, target: list) -> None:
+        pass
+    
+    @abstractmethod
+    def predict(self, input_data: list) -> list:
+        pass
+    
+    @abstractmethod
+    def save_model(self, model_path: str) -> None:
+        pass
+    
+    @abstractmethod
+    def load_model(self, model_path: str) -> None:
+        pass
+    
+    @abstractmethod
+    def add_layer(self, layer: AbstractLayer) -> None:
+        pass
