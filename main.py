61e068f03e63bc418f57928c860b7515df784657
Deciptikon <deciptikon@mail.ru>
2024-01-03 22:26:34 +0300

    добавлены классы нейронов и связей с некоторыми методами

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import time
 
 class AbstractLayer(ABC):
     
@@ -33,17 +34,58 @@ class AbstractSNN(ABC):
     @abstractmethod
     def add_layer(self, layer: AbstractLayer) -> None:
         pass
+
+
+class Link:
+    def __init__(self, length: int = 1, input_obj: 'Neuron' = None) -> None:
+        self.link: list[bool] = [False] * length
+        self.input_obj: Neuron = input_obj
+        self.T_links: list[list[Link]] = []
     
+    def step(self) -> None:
+        self.link = [False] + self.link[:-1]
+        
+    def step2(self) -> None:
+        self.link.insert(0, False)
+        self.link.pop()
+    
+    def print(self, prefix: str = ''):
+        print(f'{prefix}{self.link}')
+        
+    def set_input_obj(self, input_obj: 'Neuron' = None) -> None:
+        self.input_obj = input_obj
+    
+    def set_length(self, length: int = 1) -> None:
+        self.link = [False] * length
+        
+    def set_T_link(self, link: 'Link' = None, position: int = 0):
+        if position < len(self.link):
+            pass
     
 class Neuron:
     def __init__(self) -> None:
         input_links: list[Link] = []
         output_links: list[Link] = []
     
+    def app_input_link(self, link: Link) -> None:
+        self.input_links.append(link)
+ 
+    
 
-class Link:
-    def __init__(self) -> None:
-        input_obj: Neuron = None
-        output_obj: Neuron | [Link, int] = None
+link = Link(length=7)
+T_link = Link(length=7)
+
+link.link.insert(2, True)
+T_link.link.insert(2, True)
+
+link.print(prefix='link   = ')
+T_link.print(prefix='T-link = ')
+
+
+link.step()
+T_link.step()
+
+link.print(prefix='link   = ')
+T_link.print(prefix='T-link = ')
     
     
